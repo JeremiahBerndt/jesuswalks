f275ff8274bd45c92bc86e95ebd335d17012b04c
Jeremiah <JeremiahBerndt@gmail.com>
2022-01-11 12:53:11 -0700

    image for readme

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -9,11 +9,10 @@ export default function Answer ({ setShow, show, correct }) {
   return (
     <>
       <Modal show={show} onHide={handleClose} size="lg">
-        <Modal.Body className="answer-modal">{correct}</Modal.Body>
+        <Modal.Body style={{backgroundColor: correct === 'Incorrect' ? "red" : 'green'}}
+        className="answer-modal">{correct}</Modal.Body>
         <Modal.Footer>
-          <Button className="answer-modal" variant="warning" onClick={handleClose}>
-            Close
-          </Button>
+          <Button variant="warning" onClick={handleClose}>Close</Button>
         </Modal.Footer>
       </Modal>
     </>
